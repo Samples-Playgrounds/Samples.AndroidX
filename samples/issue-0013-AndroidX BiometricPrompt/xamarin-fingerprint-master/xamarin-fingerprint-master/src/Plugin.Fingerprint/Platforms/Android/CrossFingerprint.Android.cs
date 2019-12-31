9963c8c8fe0fb04e332379748943ac3f1bc2196f
moljac <mcvjetko@holisticware.net>
2019-12-31 14:13:35 +0100

    issue 0013 Biomtric Prompt

---
+++ samples/issue-0013-AndroidX BiometricPrompt/xamarin-fingerprint-master/xamarin-fingerprint-master/src/Plugin.Fingerprint/Platforms/Android/CrossFingerprint.Android.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Android.App;
+
+namespace Plugin.Fingerprint
+{
+    public partial class CrossFingerprint
+    {
+        private static Func<Activity> _activityResolver;
+
+        public static Activity CurrentActivity => GetCurrentActivity();
+        
+        public static void SetCurrentActivityResolver(Func<Activity> activityResolver)
+        {
+            _activityResolver = activityResolver;
+        }
+
+        private static Activity GetCurrentActivity()
+        {
+            if (_activityResolver is null)
+                throw new InvalidOperationException("Resolver for the current activity is not set. Call Fingerprint.SetCurrentActivityResolver somewhere in your startup code.");
+
+            var activity = _activityResolver();
+            if (activity is null)
+                throw new InvalidOperationException("The configured CurrentActivityResolver returned null. " +
+                                                    "You need to setup the Android implementation via CrossFingerprint.SetCurrentActivityResolver(). " +
+                                                    "If you are using CrossCurrentActivity don't forget to initialize it, too!");
+
+            return activity;
+        }
+    }
+}
