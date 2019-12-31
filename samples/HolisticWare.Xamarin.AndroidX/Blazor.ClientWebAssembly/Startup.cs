442db95316f121758827ceb63efc7c47ed1d6982
moljac <mcvjetko@holisticware.net>
2019-12-31 17:33:44 +0100

    local storage added (files)

--- samples/HolisticWare.Xamarin.AndroidX/Blazor.ClientWebAssembly/Startup.cs
+++ samples/HolisticWare.Xamarin.AndroidX/Blazor.ClientWebAssembly/Startup.cs
@@ -1,17 +1,24 @@
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
+using Blazored.LocalStorage;
+
 namespace Blazor.ClientWebAssembly
 {
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddBlazoredLocalStorage();
+
+            return;
         }
 
         public void Configure(IComponentsApplicationBuilder app)
         {
             app.AddComponent<App>("app");
+
+            return;
         }
     }
 }
