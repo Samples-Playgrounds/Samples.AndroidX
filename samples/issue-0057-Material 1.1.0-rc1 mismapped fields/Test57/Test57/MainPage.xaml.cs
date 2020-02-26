bb757a31ef16e9541d234d8de31255e4d84a2ac9
moljac <mcvjetko@holisticware.net>
2020-02-26 12:06:50 +0100

    issue 0057

---
+++ samples/issue-0057-Material 1.1.0-rc1 mismapped fields/Test57/Test57/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Test57
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Color color_text = this.entry.TextColor;
+            Color color_background = this.entry.BackgroundColor;
+
+            Entry e = this.entry;
+
+            return;
+        }
+    }
+}
