28282d8f82eacfd1f0d35b071e51f663fae23d3c
moljac <mcvjetko@holisticware.net>
2020-07-08 10:57:32 +0200

    issue 0077

---
+++ samples/issues/0077-Room/issue/XamarinProjects-master/RoomExample/INoteDao.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+using AndroidX.Room;
+
+namespace RoomExample
+{
+    [Dao]
+    public interface INoteDao
+    {
+        [Insert(OnConflict = OnConflictStrategy.Replace)]
+        void Save(Note note);
+
+        [Query(Value = "SELECT * FROM notes WHERE id = :noteId LIMIT 1")]
+        Note Load(int noteId);
+
+        [Query(Value = "SELECT * FROM notes ORDER BY id DESC")]
+        List<Note> Load();
+
+        [Update(OnConflict = OnConflictStrategy.Replace)]
+        void Update(Note note);
+
+        [Delete]
+        void Delete(Note note);
+    }
+}
---
+++ samples/issues/0077-Room/issue/XamarinProjects-master/RoomExample/NoteDetailsActivity.cs
@@ -0,0 +1,49 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System.Threading.Tasks;
+
+namespace RoomExample
+{
+    [Activity(Label = "Note Details", Theme = "@style/AppTheme", ParentActivity = typeof(MainActivity))]
+    public class NoteDetailsActivity : AppCompatActivity
+    {
+        private Note note;
+        private TextView text;
+        private TextView date;
+
+        protected async override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_note_details);
+
+            if(SupportActionBar != null)
+            {
+                SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+            }
+
+            text = FindViewById<TextView>(Resource.Id.noteDetailsText);
+            date = FindViewById<TextView>(Resource.Id.noteDetailsDate);
+
+            // Create your application here
+            var noteId = Intent.GetLongExtra("noteId", -1);
+
+            if(noteId == -1)
+            {
+                return;
+            }
+            await Task.Run(async () =>
+            {
+                // Retrieve data
+                //note = NoteDatabase.GetInstance(this).NoteDao().Load(noteId);
+            });
+
+            //text.Text = note.Text;
+            //date.Text = note.Timestamp.ToString();
+            text.Text = $"Note id {noteId}";
+            date.Text = System.DateTime.Now.ToString();
+        }
+    }
+}
