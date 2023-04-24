f1a11e3bd47cd6cdb8b7051916cab0a39b82a68f
moljac <mcvjetko@holisticware.net>
2023-04-24 12:06:21 +0200

    issue 717 tbalcom

---
+++ samples/issues-repro-samples/717-dupllicate-classes/user-tbalcom/AppMAUI.Issue717.DuplicateClasses/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AppMAUI.Issue717.DuplicateClasses;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
---
+++ samples/issues-repro-samples/717-dupllicate-classes/user-tbalcom/readme.md
@@ -0,0 +1,7 @@
+# Issue 717 Duplicate classes
+
+*   https://github.com/xamarin/AndroidX/issues/717
+
+
+# Step 01 - use users package references
+
